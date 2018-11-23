291aebcc717c666a42350875ba045835f43fc68e
moustafarefaat <$cala5B@sha!>
2018-11-23 09:08:39 -0500

    Updated the format to be Templated now the real fun would start

--- src/main/scala/com/GeneticThought/Algorithms/MainApp.scala
+++ src/main/scala/com/GeneticThought/Algorithms/MainApp.scala
@@ -1,11 +1,10 @@
 package com.GeneticThought.Algorithms
 
-import  com.GeneticThought.Algorithms.Sorting.insertSort
+
 
 object MainApp extends App {
   println ("Testing insert Sorting ")
   val ar : Array[Long] = Array(10L, 20L,40L, 60L , 7L )
-  insertSort(ar)
+  Sorting.insertSortLong(ar)
   ar.foreach(println)
-
 }
--- src/main/scala/com/GeneticThought/Algorithms/Sorting.scala
+++ src/main/scala/com/GeneticThought/Algorithms/Sorting.scala
@@ -1,16 +1,28 @@
 package com.GeneticThought.Algorithms
 
-object Sorting {
+import scala.math
 
-  def insertSort(array: Array[Long]): Unit = {
-    for (j <- 1 to array.size-1) {
+class Sorting[T] {
+ /* type OrderedView[T] = T=> Ordered[T]
+  def compareGreater[T: OrderedView](o1: T, o2: T): Boolean = {
+    o1 > o2
+  }
+*/
+  def insertSort(array: Array[T], greaterThan: (T,T) => Boolean): Unit = {
+    for (j <- 1 to array.size - 1) {
       val key = array(j)
       var i = j - 1
-      while (i >= 0 && array(i) > key) {
+      while (i >= 0 && greaterThan(array(i), key)) {
         array(i + 1) = array(i)
         i -= 1
       }
       array(i + 1) = key
     }
   }
+}
+
+object Sorting {
+
+   def insertSortLong(array:Array[Long]): Unit = (new Sorting[Long]()).insertSort(array = array,(a,b) => a >b)
+
 }
